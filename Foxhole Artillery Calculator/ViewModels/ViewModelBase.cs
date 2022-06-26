66b506759a825ff06dccca186898236c6db72839
Colin Hartgers <chartgers2014@gmail.com>
2022-06-25 23:26:00 -0400

    Initial commit

---
+++ Foxhole Artillery Calculator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foxhole_Artillery_Calculator.ViewModels
+{
+	public class ViewModelBase : ReactiveObject
+	{
+	}
+}
---
+++ Foxhole Artillery Calculator/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Foxhole_Artillery_Calculator.Views
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
