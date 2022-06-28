55306872768a6d3839ce0d0d0ec4a68a58132503
Colin Hartgers <chartgers2014@gmail.com>
2022-06-27 22:34:37 -0400

    Create Gun/Target Creator and Wind Offset panels

--- Foxhole Artillery Calculator/ViewModels/MainWindowViewModel.cs
+++ Foxhole Artillery Calculator/ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,4 @@
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,6 +7,17 @@ namespace Foxhole_Artillery_Calculator.ViewModels
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
-		public string Greeting => "Welcome to Avalonia!";
+		double _distance;
+		double _azimuth;
+		public double Distance
+		{
+			get => _distance;
+			set => this.RaiseAndSetIfChanged(ref _distance, value);
+		}
+		public double Azimuth
+		{
+			get => _azimuth;
+			set => this.RaiseAndSetIfChanged(ref _azimuth, ((value % 360) + 360) % 360);
+		}		
 	}
 }
